68b58b5ce8f7b09e0729e0bb0bc2b2987acd4049
Leonardo Rossi <leonardo.rossi@unipr.it>
2021-01-15 12:20:23 +0100

    run: disable useless predict on fake test set
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ python3 main.py \
     --task='sequence_labelling' \
     --embedding='general_character_bert' \
     --do_lower_case \
-    --do_train \
-    --do_predict
+    --do_train # \
+    # --do_predict
