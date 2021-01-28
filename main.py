31ffb1e8ba3cfc94f4e652a764aea1db2bb1ea9d
Leonardo Rossi <leonardo.rossi@unipr.it>
2021-01-28 13:27:41 +0100

    main: weighted loss
    Signed-off-by: Leonardo Rossi <leonardo.rossi@unipr.it>

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from torch.nn import CrossEntropyLoss
 from transformers import (
     BertConfig,
     BertTokenizer,
-    BertForTokenClassification,
+    BertForTokenClassification as BFTC,
     BertForSequenceClassification
 )
 from utils.character_cnn import CharacterIndexer
@@ -28,6 +28,41 @@ from utils.training import train, evaluate
 
 from download import MODEL_TO_URL
 AVAILABLE_MODELS = list(MODEL_TO_URL.keys()) + ['bert-base-uncased']
+
+
+class BertForTokenClassification(BFTC):
+
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None,
+                position_ids=None, head_mask=None, inputs_embeds=None, labels=None):
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        sequence_output = outputs[0][0]
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+
+        outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
+        #  import ipdb; ipdb.set_trace()
+        if labels is not None:
+            weights = torch.tensor([1., 0.514, 0.028]).to(logits)
+            loss_fct = CrossEntropyLoss(weight=weights)
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)[active_loss]
+                active_labels = labels.view(-1)[active_loss]
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            outputs = (loss,) + outputs
+
+        return outputs  # (loss), scores, (hidden_states), (attentions)
+
 
 def analysis(data):
     def count(data_):
